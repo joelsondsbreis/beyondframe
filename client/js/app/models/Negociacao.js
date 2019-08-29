e46672131fa031a70d03db53a8e6c16aa4eaa246
Joelson Reis <joelsondsbreis@gmail.com>
2019-08-29 13:01:56 -0300

    Programação defensiva

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -5,12 +5,13 @@ class Negociacao{
         this._data = new date();
         this._quantidade = 1;
         this._valor = 0.0;
+        Object.freeze(this);
     }
     get volume(){
         return this._quantidade * this._valor;   
     }
     get data(){
-        return this._data;
+        return new Date(this._data.getTime());
     }
     get quantidade(){
         return this._quantidade;
