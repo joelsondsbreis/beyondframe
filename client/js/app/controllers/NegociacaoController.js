a800d718f87616dc1811470169d8ff60e9e7ed1f
joelsondsbreis <50884925+joelsondsbreis@users.noreply.github.com>
2019-09-02 22:00:26 -0300

    Commit 05

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+class NegociacaoController{
+
+    adiciona(event){
+        event.preventDefault();
+
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+    }
+}
