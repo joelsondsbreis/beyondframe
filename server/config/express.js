099bf3322fba8552d04b94997d1e304b61313c1d
Joelson Reis <joelsondsbreis@gmail.com>
2019-09-11 13:15:29 -0300

    update

--- server/config/express.js
+++ server/config/express.js
@@ -1,4 +1,4 @@
-/* Código simples, apenas para fornecer o serviço para a aplicação */
+/* Código simplório, apenas para fornecer o serviço para a aplicação */
 
 var express = require('express')
     ,app = express()
